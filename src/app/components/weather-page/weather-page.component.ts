81031d70e2bfed4565298b1b60a051fe715f7f88
Marc Gervacio <gervaciomarc@gmail.com>
2024-04-21 00:14:55 +0800

    Setup Style for Weather Page

--- src/app/components/weather-page/weather-page.component.ts
+++ src/app/components/weather-page/weather-page.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { WeatherDetails } from '../../models/weather-details.model';
+import { MatTableModule } from '@angular/material/table'
 
 @Component({
   selector: 'app-weather-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, MatTableModule],
   templateUrl: './weather-page.component.html',
   styleUrl: './weather-page.component.scss'
 })
-export class WeatherPageComponent {
+export class WeatherPageComponent implements OnInit {
+  /** Weather details to be displayed */
+  weatherDetails!: WeatherDetails;
 
+  /** Table columns to be displayed */
+  displayedColumns: string[] = ['date', 'temperature', 'description', 'main', 'pressure', 'humidity'];
+
+  /** Data to be displayed in the table details */
+  dataSource!: any[];
+
+  ngOnInit(): void {
+    this.setWeatherDetails();
+    this.dataSource = [this.weatherDetails];
+  }
+
+  /** Set the value for the weatherDetails */
+  setWeatherDetails(): void {
+    this.weatherDetails = {
+      cityName: 'Sample City',
+      date: new Date(),
+      temperature: 'Sample Temperature',
+      description: 'Sample Description',
+      main: 'Sample Main',
+      pressure: 'Sample Pressure',
+      humidity: 'Sample Humidity'
+    }
+  }
 }
---
+++ src/app/models/weather-details.model.ts
@@ -0,0 +1,9 @@
+export interface WeatherDetails {
+    cityName: string;
+    date: Date;
+    temperature: string;
+    description: string;
+    main: string;
+    pressure: string;
+    humidity: string;
+}
